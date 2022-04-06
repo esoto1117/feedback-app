8cb04392fb4e40502655ddc852267ed6313f6293
Eddie Soto <esoto1117@gmail.com>
2022-04-05 23:38:50 -0400

    validation on form text, less or more than 10 characters

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,14 @@ import Button from "./shared/Button"
 function FeedbackForm() {
 
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
+    if(text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    }
     setText(e.target.value)
   }
 
@@ -17,8 +23,10 @@ function FeedbackForm() {
         {/* @todo - rating select component */}
         <div className="input-group">
           <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
