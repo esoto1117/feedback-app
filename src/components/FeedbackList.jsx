5ab41880ac7f3cd7e18d77a914d4f42e5a1ad45e
Eddie Soto <esoto1117@gmail.com>
2022-04-11 21:49:19 -0400

    add feedback

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -6,6 +6,7 @@ function FeedbackList({feedback, handleDelete}) {
   if(!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
+  
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
